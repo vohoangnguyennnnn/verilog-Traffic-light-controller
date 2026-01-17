7f28ed6a81d0af932ee4318f0be442f08f9c74a8
nguyen-24805 <nguyenpktdbha@gmail.com>
2026-01-17 19:29:13 +0700

    Update

---
+++ Testbench/traffic_light_tb.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 01/17/2026 07:26:39 PM
+// Design Name: 
+// Module Name: traffic_light_tb
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module traffic_light_tb(
+
+    );
+endmodule
---
+++ docs/Report.md
@@ -0,0 +1,2 @@
+# verilog-Traffic-light-controller
+Verilog-based traffic light controller implemented as a finite state machine (FSM), including RTL design, simulation, and testbench verification.
