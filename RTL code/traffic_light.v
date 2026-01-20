8842a5d1f58495161a8de0f5b14b8e618f463e1d
nguyen-24805 <nguyenpktdbha@gmail.com>
2026-01-20 18:31:26 +0700

    Update RTL verilog code

--- RTL code/traffic_light.v
+++ RTL code/traffic_light.v
@@ -20,7 +20,75 @@
 //////////////////////////////////////////////////////////////////////////////////
 
 
-module traffic_light(
-
+module traffic_light #(
+    parameter integer RED_TIME = 15,   // thời gian state tồn tại
+    parameter integer YELLOW_TIME = 4,
+    parameter integer GREEN_TIME = 11
+    )(
+    input wire clk,
+    input wire rst,
+    output reg red,
+    output reg yellow,
+    output reg green
     );
+    
+    localparam[1:0] STATE_RED = 2'd0;
+    localparam[1:0] STATE_YELLOW = 2'd1;
+    localparam[1:0] STATE_GREEN = 2'd2;
+    
+    reg[1:0] state, next_state;
+    reg[15:0] cnt, next_cnt;
+    
+    //Combinational logic
+    always @(*) begin
+    //defaults
+    next_state = state;
+    next_cnt = cnt;
+    red = 1'b0;
+    yellow = 1'b0;
+    green = 1'b0;
+    
+    case (state)
+        STATE_RED: begin    // Red light
+            red = 1'b1;
+            if (cnt == RED_TIME - 1) begin
+                next_state = STATE_YELLOW;
+                next_cnt = 16'd0;
+            end else next_cnt = cnt + 1;
+        end
+        
+        STATE_YELLOW: begin  // Yellow light
+            yellow = 1'b1;
+            if (cnt == YELLOW_TIME - 1) begin
+                next_state = STATE_GREEN;
+                next_cnt = 16'd0;
+            end else next_cnt = cnt + 1;
+        end
+        
+        STATE_GREEN: begin  // Green light
+            green = 1'b1;
+            if (cnt == GREEN_TIME - 1) begin
+                next_state = STATE_RED;
+                next_cnt = 16'd0;
+            end else next_cnt = cnt + 1;
+        end 
+        
+        default: begin
+            red = 1'b1;
+            next_state = STATE_RED;
+            next_cnt = 16'd0;
+        end
+     endcase
+   end
+   
+   //Sequential logic
+   always @(posedge clk) begin
+        if (rst) begin
+            state <= STATE_RED;
+            cnt <= 16'd0;
+        end else begin
+            state <= next_state;
+            cnt <= next_cnt;
+        end
+    end
 endmodule
